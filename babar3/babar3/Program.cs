77fdf9fda4f2f3238e588f7adeaac44c4df63fe8
1 <1>
2016-10-21 14:35:59 +0300

    commit 3 gg

---
+++ babar3/babar3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace babar3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("go commit 3");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
