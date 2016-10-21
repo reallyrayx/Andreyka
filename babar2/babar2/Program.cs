495b5acee0a5032c9819f9d2be05c2ab5880c7b0
1 <1>
2016-10-21 14:28:12 +0300

    commit 2 program ready

---
+++ babar2/babar2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace babar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program ready");
+
+        
+            Console.WriteLine("Tab to exit");
+            Console.ReadKey();
+        }
+    }
+}
